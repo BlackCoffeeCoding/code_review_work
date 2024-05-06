3fa7ac95517dc5d31446c7e50a0f12bf777f8106
BlackCoffeeCoding <92050880+BlackCoffeeCoding@users.noreply.github.com>
2024-05-06 13:50:52 +0300

    Update CalculatorTest.java

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -7,46 +7,29 @@ class CalculatorTest {
 
     @Test
     void add() {
-        assertEquals(8,calculator.add(5,3));
-        //факинг фак
-
-
-    @Test
-    void add() {
         assertEquals(0,calculator.add(-1,1));
 
     }
 
     @Test
     void dif() {
-
-        assertEquals(0,calculator.dif(1,1));
-
         assertEquals(-2,calculator.dif(-1,1));
-
     }
 
     @Test
     void div() {
-
-        assertEquals(6,calculator.div(12,2));
-
         assertEquals(0,calculator.div(0,1));
 
     }
 
     @Test
     void times() {
-
-        assertEquals(4,calculator.times(2,2));
-
         assertEquals(-1,calculator.times(-1,1));
 
     }
 
     @Test
     void solver() {
-
 
         assertEquals(7,calculator.solver());
 

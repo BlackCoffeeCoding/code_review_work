a75b7fc2167ac0ab7d4c8517d38801fc917e4b3e
BlackCoffeeCoding <92050880+BlackCoffeeCoding@users.noreply.github.com>
2024-05-06 13:49:43 +0300

    Update Calculator.java

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,24 +1,5 @@
 public class Calculator {
     public int add(int a, int b){
-        System.out.println(a+b);
-        return 0;
-    }
-    public int dif(int a, int b){
-        System.out.println(a-b);
-        return 0;
-        //факинг фак
-    }
-    public int div(int a, int b){
-        System.out.println(a*b);
-        return 0;
-    }
-    public int times(int a, int b){
-        System.out.println(a/b);
-        return 0;
-    }
-    public int solver(){
-        int a=3;
-        int b=6;
         return a+b;
     }
     public int dif(int a, int b){
